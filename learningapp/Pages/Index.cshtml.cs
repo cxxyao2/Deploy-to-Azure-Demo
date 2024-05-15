b52f82f5da51f7d812bc32b2f31267ea68d1d758
jennifer <cxxyao2@gmail.com>
2024-05-14 22:04:51 -0300

    get data from azure functions'

--- learningapp/Pages/Index.cshtml.cs
+++ learningapp/Pages/Index.cshtml.cs
@@ -1,37 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace learningapp.Pages;
 
 public class IndexModel : PageModel
 {
-     public List<Course> Courses=new List<Course>();
+    public List<Course> Courses = new List<Course>();
     private readonly ILogger<IndexModel> _logger;
     private IConfiguration _configuration;
-    public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
-        _configuration=configuration;
+        _configuration = configuration;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet()
     {
-       
-        string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
-        var sqlConnection = new SqlConnection(connectionString);
-        sqlConnection.Open();
+        string functionUrl = "https://appfunction454545.azurewebsites.net/api/sqltrigger";
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetAsync(functionUrl);
 
-        var sqlcommand = new SqlCommand(
-        "SELECT CourseID,CourseName,Rating FROM Course;",sqlConnection);
-         using (SqlDataReader sqlDatareader = sqlcommand.ExecuteReader())
-         {
-             while (sqlDatareader.Read())
-                {
-                    Courses.Add(new Course() {CourseID=Int32.Parse(sqlDatareader["CourseID"].ToString()),
-                    CourseName=sqlDatareader["CourseName"].ToString(),
-                    Rating=Decimal.Parse(sqlDatareader["Rating"].ToString())});
-                }
-         }
+            var data = await response.Content.ReadAsStringAsync();
+            Courses = JsonConvert.DeserializeObject<List<Course>>(data);
+            return Page();
+
+        }
+
     }
 }
